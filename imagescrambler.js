c14e73d8ab53d7939cf342e5a5159bbf0d49c253
SpotsBoi <159225484+SpotsBoi@users.noreply.github.com>
2024-03-21 10:53:41 -0700

    Moved image scrambling code to its own file

---
+++ imagescrambler.js
@@ -0,0 +1,110 @@
+const COLOR_COMPONENTS = 4;
+const IV_SIZE = 16;
+
+function BlockToPixelIndex(imageData, blockIndex, blockSize) {
+    let imageBlockWidth = imageData.width / blockSize;
+
+    return {
+        x: (blockIndex % imageBlockWidth) * blockSize,
+        y: Math.floor(blockIndex / imageBlockWidth) * blockSize
+    };
+}
+
+function GetPixelIndex(imageData, x, y) {
+    return (y * imageData.width + x) * COLOR_COMPONENTS;
+}
+
+function SwapBlocks(imageData, block1, block2, blockSize) {
+    if (blockSize === 1) {
+        let block1Index = block1 * COLOR_COMPONENTS;
+        let block2Index = block2 * COLOR_COMPONENTS;
+
+        for (let i = 0; i < COLOR_COMPONENTS; i++) {
+            let tempColor = imageData.data[block1Index + i];
+            imageData.data[block1Index + i] = imageData.data[block2Index + i];
+            imageData.data[block2Index + i] = tempColor;
+        }
+    } else {
+        let block1Start = BlockToPixelIndex(imageData, block1, blockSize);
+        let block2Start = BlockToPixelIndex(imageData, block2, blockSize);
+
+        for (let yOffset = 0; yOffset < blockSize; yOffset++) {
+            for (let xOffset = 0; xOffset < blockSize; xOffset++) {
+                let block1Index = GetPixelIndex(imageData, block1Start.x + xOffset, block1Start.y + yOffset);
+                let block2Index = GetPixelIndex(imageData, block2Start.x + xOffset, block2Start.y + yOffset);
+
+                for (let i = 0; i < COLOR_COMPONENTS; i++) {
+                    let tempColor = imageData.data[block1Index + i];
+                    imageData.data[block1Index + i] = imageData.data[block2Index + i];
+                    imageData.data[block2Index + i] = tempColor;
+                }
+            }
+        }
+    }
+}
+
+async function ScrambleImage(targetImage, targetCanvas, password, unscramble, blockSize) {
+    const context = targetCanvas.getContext("2d");
+
+    // Resize the canvas to fit the image.
+    if (targetImage.width % blockSize === 0)
+        targetCanvas.width = targetImage.width;
+    else
+        targetCanvas.width = targetImage.width + blockSize - (targetImage.width % blockSize);
+
+    if (targetImage.height % blockSize === 0)
+        targetCanvas.height = targetImage.height;
+    else
+        targetCanvas.height = targetImage.height + blockSize - (targetImage.height % blockSize);
+
+    // Get the image data.
+    context.fillStyle = "white";
+    context.fillRect(0, 0, targetCanvas.width, targetCanvas.height);
+    context.drawImage(targetImage, 0, 0);
+    let imageData = context.getImageData(0, 0, targetCanvas.width, targetCanvas.height);
+
+    let blockCount = (targetCanvas.width * targetCanvas.height) / (blockSize * blockSize);
+
+    // Hash the password.
+    let hashedPassword = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(password));
+    let iv = new Uint8Array(IV_SIZE);
+    let key = await crypto.subtle.importKey("raw", hashedPassword, "AES-CBC", false, ["encrypt"]);
+
+    // Determine loop bounds and increments.
+    let blockStart, blockEnd, inc;
+
+    if (unscramble) {
+        blockStart = blockCount - 1;
+        blockEnd = -1;
+        inc = -1;
+    } else {
+        blockStart = 0;
+        blockEnd = blockCount;
+        inc = 1;
+    }
+
+    // Generate random numbers.
+    let randomNumbers = new Uint32Array(blockCount);
+    
+    randomNumbers = await crypto.subtle.encrypt(
+        {
+            name: "AES-CBC",
+            iv: iv,
+        },
+        key,
+        randomNumbers
+    );
+    
+    // Turn the random numbers into pixel indices.
+    randomNumbers = new Uint32Array(randomNumbers);
+    for (let i = 0; i < randomNumbers.length; i++)
+        randomNumbers[i] = randomNumbers[i] % blockCount;
+
+    // Scramble or unscramble the pixels.
+    for (let j = blockStart; j != blockEnd; j += inc) {
+        SwapBlocks(imageData, j, randomNumbers[j], blockSize);
+    }
+
+    // Put the result back on the canvas.
+    context.putImageData(imageData, 0, 0);
+}
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const COLOR_COMPONENTS = 4;
-
 window.onload = function() {
     // Get HTML elements.
     const imageURLContainer = document.getElementById("imageurlcontainer");
@@ -23,67 +21,24 @@ window.onload = function() {
     downloadLink.download = "imagescrambleroutput.png";
 
     const canvas = document.getElementById("imagecanvas");
-    const context = canvas.getContext("2d");
 
     // Global state
     let useImageURL = true;
-    let blockSize = 1;
+    let blockSizeGlobal = 1;
 
     function UpdateBlockSize() {
-        blockSize = Math.pow(2, parseInt(blockSizeSilder.value));
-        blockSizeText.innerHTML = "Block Size: " + blockSize;
+        blockSizeGlobal = Math.pow(2, parseInt(blockSizeSilder.value));
+        blockSizeText.innerHTML = "Block Size: " + blockSizeGlobal;
     }
 
-    function BlockToPixelIndex(imageData, blockIndex) {
-        let imageBlockWidth = imageData.width / blockSize;
-
-        return {
-            x: (blockIndex % imageBlockWidth) * blockSize,
-            y: Math.floor(blockIndex / imageBlockWidth) * blockSize
-        };
-    }
-
-    function GetPixelIndex(imageData, x, y) {
-        return (y * imageData.width + x) * COLOR_COMPONENTS;
-    }
-
-    function SwapBlocks(imageData, block1, block2) {
-        if (blockSize === 1) {
-            let block1Index = block1 * COLOR_COMPONENTS;
-            let block2Index = block2 * COLOR_COMPONENTS;
-
-            for (let i = 0; i < COLOR_COMPONENTS; i++) {
-                let tempColor = imageData.data[block1Index + i];
-                imageData.data[block1Index + i] = imageData.data[block2Index + i];
-                imageData.data[block2Index + i] = tempColor;
-            }
-        } else {
-            let block1Start = BlockToPixelIndex(imageData, block1);
-            let block2Start = BlockToPixelIndex(imageData, block2);
-
-            for (let yOffset = 0; yOffset < blockSize; yOffset++) {
-                for (let xOffset = 0; xOffset < blockSize; xOffset++) {
-                    let block1Index = GetPixelIndex(imageData, block1Start.x + xOffset, block1Start.y + yOffset);
-                    let block2Index = GetPixelIndex(imageData, block2Start.x + xOffset, block2Start.y + yOffset);
-
-                    for (let i = 0; i < COLOR_COMPONENTS; i++) {
-                        let tempColor = imageData.data[block1Index + i];
-                        imageData.data[block1Index + i] = imageData.data[block2Index + i];
-                        imageData.data[block2Index + i] = tempColor;
-                    }
-                }
-            }
-        }
-    }
-
-    function ScrambleImage(imageURL, password, unscramble) {
+    function FetchAndScrambleImage(unscramble) {
         // Create a new image.
         let targetImage = new Image();
 
         // Fetch the image.
         if (useImageURL) {
             targetImage.crossOrigin = "";
-            targetImage.src = imageURL;
+            targetImage.src = imageURLInput.value;
         } else {
             if (imageFileInput.files.length === 0) {
                 alert("Please upload a file.");
@@ -102,68 +57,8 @@ window.onload = function() {
             alert("Failed to load image.");
         }
 
-        targetImage.onload = async function() {
-            // Resize the canvas to fit the image.
-            if (targetImage.width % blockSize === 0)
-                canvas.width = targetImage.width;
-            else
-                canvas.width = targetImage.width + blockSize - (targetImage.width % blockSize);
-
-            if (targetImage.height % blockSize === 0)
-                canvas.height = targetImage.height;
-            else
-                canvas.height = targetImage.height + blockSize - (targetImage.height % blockSize);
-
-            // Get the image data.
-            context.fillStyle = "white";
-            context.fillRect(0, 0, canvas.width, canvas.height);
-            context.drawImage(targetImage, 0, 0);
-            let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-
-            let blockCount = (canvas.width * canvas.height) / (blockSize * blockSize);
-
-            // Hash the password.
-            let hashedPassword = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(password));
-            let iv = new Uint8Array(16);
-            let key = await crypto.subtle.importKey("raw", hashedPassword, "AES-CBC", false, ["encrypt"]);
-
-            // Determine loop bounds and increments.
-            let blockStart, blockEnd, inc;
-
-            if (unscramble) {
-                blockStart = blockCount - 1;
-                blockEnd = -1;
-                inc = -1;
-            } else {
-                blockStart = 0;
-                blockEnd = blockCount;
-                inc = 1;
-            }
-
-            // Generate random numbers.
-            let randomNumbers = new Uint32Array(blockCount);
-            
-            randomNumbers = await crypto.subtle.encrypt(
-                {
-                    name: "AES-CBC",
-                    iv: iv,
-                },
-                key,
-                randomNumbers
-            );
-            
-            // Turn the random numbers into pixel indices.
-            randomNumbers = new Uint32Array(randomNumbers);
-            for (let i = 0; i < randomNumbers.length; i++)
-                randomNumbers[i] = randomNumbers[i] % blockCount;
-
-            // Scramble or unscramble the pixels.
-            for (let j = blockStart; j != blockEnd; j += inc) {
-                SwapBlocks(imageData, j, randomNumbers[j]);
-            }
-
-            // Put the result back on the canvas.
-            context.putImageData(imageData, 0, 0);
+        targetImage.onload = function() {
+            ScrambleImage(targetImage, canvas, passwordInput.value, unscramble, blockSizeGlobal);
         }
     }
 
@@ -188,11 +83,11 @@ window.onload = function() {
     blockSizeSilder.oninput = UpdateBlockSize;
 
     scrambleButton.onclick = function() {
-        ScrambleImage(imageURLInput.value, passwordInput.value, false);
+        FetchAndScrambleImage(false);
     }
 
     unscrambleButton.onclick = function() {
-        ScrambleImage(imageURLInput.value, passwordInput.value, true);
+        FetchAndScrambleImage(true);
     }
 
     saveImageButton.onclick = function() {
